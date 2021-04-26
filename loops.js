1093ee1a45911b29977208e9680c72cc298d24e9
zstapel <70587449+zstapel@users.noreply.github.com>
2021-04-26 14:32:58 -0500

    loops workshop

---
+++ loops.js
@@ -0,0 +1,28 @@
+// const names = ['john', 'bob', 'mary', 'joe'];
+
+// // for(let i = 0; i <  names.length; i++){
+// // 	console.log(names[i]);
+// // 	// if(i === 3) break;
+
+// // }
+
+// // for (name of names) console.log(name);
+
+// const user =  {'firstName': 'John', 'lastName': 'Doe'}
+// for(key in user) console.log(user[key]);
+
+// let i = 0;
+// do {
+// 	i++;
+// 	if(i == 5) continue; 
+// 	console.log(i);
+// }while(i < 0) 
+	
+	// continue:like a break so skips over this and then continues
+	// break: stops completly
+	
+const animals = ['cat', 'dog', 'horse', 'sheep', 'bird'];
+
+animals.forEach( animal =>{
+	console.log(animal);
+})
